ba9cf2b89de768357f3745b65d8c251996c5b837
hodygmd <gmdhody@gmail.com>
2023-12-16 01:38:53 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import ComponentePrueba from "./componentes/ComponentePrueba";
+
+function App() {
+  return (
+    <ComponentePrueba/>
+  );
+}
+
+export default App;
