2f0d061e0708c23da55af2d6f6845fe7e465b0b6
hodygmd <gmdhody@gmail.com>
2024-06-29 16:51:14 -0600

    add one change

--- src/Components/GitTest.jsx
+++ src/Components/GitTest.jsx
@@ -15,10 +15,14 @@ const GitTest = () => {
         setFlag(false)
     }, [flag]);
     const [imagen, setImagen] = useState(null);
+    const [imagenUp, setImagenUp] = useState(null);
     const [nom,setNom]=useState(null)
 
     const seleccionarImagen = (e) => {
         setImagen(e.target.files[0]);
+    };
+    const seleccionarImagenUp = (e) => {
+        setImagenUp(e.target.files[0]);
     };
     const handleNom=(event)=>{
         setNom(event.target.value)
@@ -29,7 +33,7 @@ const GitTest = () => {
         const formData = new FormData();
         formData.append("image", imagen);
         /*formData.append("nom", nom);*/
-        axios.post('http://localhost:3001/api/images',new AddData(formData.image,nom)).then(response => {
+        axios.post('http://localhost:3001/api/images',formData).then(response => {
             console.log(response)
         }).catch(error => {
             console.log(error)
@@ -44,21 +48,36 @@ const GitTest = () => {
           console.log(error)
         })
     }
+    const updateImg=(id)=>{
+        const formData = new FormData();
+        formData.append("image", imagenUp);
+        axios.put(`http://localhost:3001/api/imagesbyid/${id}`, formData).then(response => {
+            console.log(response)
+            setFlag(true)
+        }).catch(error => {
+            console.log(error)
+        })
+    }
     return (
         <div>
             <h1>Git Test</h1>
             <form onSubmit={enviarImagen}>
                 <input type={'text'} value={nom} onChange={handleNom}/><br/>
                 <input type="file" name="image" accept="image/*" onChange={seleccionarImagen}/><br/>
-                <button type="submit" className={'bg-sky-500'}>Enviar</button><br/>
+                <button type="submit" className={'bg-sky-500'}>Enviar</button>
+                <br/>
+                <input type="file" name="image" accept="image/*" onChange={seleccionarImagenUp}/><br/>
             </form>
             <br/><br/>
             <div className={'d-flex flex-row justify-around flex-wrap'}>
                 {data.map((item,index)=>(
                     <div>
                         <label>Extraida de la bd</label>
-                        <img src={`http://localhost:3001/${item.image}`} style={{width: '150px', height: '150px'}}/>
-                        <button type="button" className={'bg-red-500'} onClick={()=>deleteImg(item.id)}>Eliminar</button>
+                        <img src={`${item.image}`} style={{width: '150px', height: '150px'}}/>
+                        <button type="button" className={'bg-red-500'} onClick={() => deleteImg(item.id)}>Eliminar
+                        </button>
+                        <button type="button" className={'bg-red-500'} onClick={() => updateImg(item.id)}>update
+                        </button>
                     </div>
                 ))
                 }
